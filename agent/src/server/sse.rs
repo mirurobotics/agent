17309d67dfec3301b761e342edd26196e6a22f46
miru-agent <agents@miruml.com>
2026-04-09 14:23:15 -0700

    style(server): apply cargo fmt to sse.rs

--- agent/src/server/sse.rs
+++ agent/src/server/sse.rs
@@ -79,10 +79,9 @@ async fn events_impl(
     });
 
     let mut shutdown_rx = state.shutdown_tx.subscribe();
-    let sse_stream = futures::StreamExt::take_until(
-        sse_stream,
-        async move { let _ = shutdown_rx.recv().await; },
-    );
+    let sse_stream = futures::StreamExt::take_until(sse_stream, async move {
+        let _ = shutdown_rx.recv().await;
+    });
 
     Ok(Sse::new(sse_stream).keep_alive(
         KeepAlive::new()
